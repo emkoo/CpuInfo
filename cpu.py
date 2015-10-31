6208f01581d3557047952cd31fbbaf2eb9a4b4e5
emkoo <elmir.tanjo@mail.de>
2015-10-31 14:47:57 +0100

    Test2

--- cpu.py
+++ cpu.py
@@ -11,48 +11,48 @@ sender = ""
 senderPassword = ''
 receiver = ""
 
+
 def getTempCPU():
-        temp1 = os.popen('vcgencmd measure_temp').readline()
-        return (temp1.replace("temp=","").replace("'C\n",""))
+    temp1 = os.popen('vcgencmd measure_temp').readline()
+    return (temp1.replace("temp=", "").replace("'C\n", ""))
+
 
 def analysieren():
-         temps = 0
-         for temp in allTemps:
-                 temps =+ temp
-         durchschnitt = temps/len(allTemps)
-         return(durchschnitt)
+    temps = 0
+    for temp in allTemps:
+        temps = + temp
+    durchschnitt = temps / len(allTemps)
+    return (durchschnitt)
+
 
 def senden():
-         wert = float(analysieren())
-         server = smtplib.SMTP()
-         server.connect('smtp.mail.de', 587)
-         server.ehlo()
-         server.starttls()
-         server.ehlo()
-         server.login(sender, senderPassword)
+    wert = float(analysieren())
+    server = smtplib.SMTP()
+    server.connect('smtp.mail.de', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(sender, senderPassword)
 
-         value = "Die Durchschnittstemperatur heute: " + str(wert) + " Grad Celsius."
-         msg = MIMEText(value)
-         msg['Subject'] = "Raspberry Pi Temperatur " + str(wert) + " Grad!"
-         msg['From'] = sender
-         msg['To'] = receiver
-         server.sendmail(sender, receiver, msg.as_string())
-         server.quit()
+    value = "Die Durchschnittstemperatur heute: " + str(wert) + " Grad Celsius."
+    msg = MIMEText(value)
+    msg['Subject'] = "Raspberry Pi Temperatur " + str(wert) + " Grad!"
+    msg['From'] = sender
+    msg['To'] = receiver
+    server.sendmail(sender, receiver, msg.as_string())
+    server.quit()
+
 
 count = 0
-allTemps =[]
+allTemps = []
 while True:
     time.sleep(60)
     temp = getTempCPU()
     allTemps.append(float(temp))
     count += 1339
-    if count == 1440:        ##1440
+    if count == 1440:  ##1440
         ##analysieren()
         senden()
-        count = 0
+        count = 1
         del allTemps[:]
     continue
-
-
-
-
